86b32f355494418fb873b19d428ad74a13efdfb5
Iurii Perederii <uyuddar@gmail.com>
2022-04-06 14:50:35 +0400

    Removed comments

--- src/app/services/score-service/score.service.types.ts
+++ src/app/services/score-service/score.service.types.ts
@@ -1,15 +1,3 @@
-// export type ScoreMessageType = 'reset' | 'init' | 'increment';
-
-// type Event<Type extends ScoreMessageType, Payload = {}> = {
-//     type: Type;
-//     payload?: Payload;
-// };
-
-// export type ScoreMessage = 
-//     | Event<'reset'>
-//     | Event<'init', { total: number; }>
-//     | Event<'increment', { total: number; currentScore: number; }>
-
 export type ScoreInfo = {
     total: number;
     score: number;
